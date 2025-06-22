e1d1c75421683b267708afb307fdbd172f61099e
rawanatef634 <atron944@gmail.com>
2025-06-22 05:48:05 +0300

    updated backend url

--- frontend/src/stores/useChatStore.ts
+++ frontend/src/stores/useChatStore.ts
@@ -22,7 +22,7 @@ interface ChatStore {
 	setSelectedUser: (user: User | null) => void;
 }
 
-const baseURL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
+const baseURL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "https://spotify-clone-bx13.onrender.com";
 
 const socket = io(baseURL, {
 	autoConnect: false,
